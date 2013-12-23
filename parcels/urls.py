2f712403f35de65a40b890e7fb379f2e0355348c
Reinis Veips <reinis.veips@wot.lv>
2013-12-24 01:48:29 +0200

    Fixes #3056 Added delete/edit functionality for shipments

--- parcels/urls.py
+++ parcels/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('parcels.views',
     url(r'^list/$', 'list_parcels', name='list_parcels'),
     url(r'^add_shipment/$', 'add_parcel'),
     url(r'^shipment/(?P<shipment_id>\d+)/$', 'shipment_info', name="single_shipment"),
+    url(r'^shipment/(?P<shipment_id>\d+)/edit/$', 'shipment_edit', name="edit_shipment"),
     url(r'^search/$', 'search_parcels', name='search_parcels'),
 )
