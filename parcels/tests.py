44eace74b9e414041064b11595765d660159291e
Reinis Veips <reinis.veips@wot.lv>
2014-01-21 00:03:01 +0200

    Added a test case which tested if Shipment.is_received is set correctly. It did not turn out to be a problem- Bootstrap simply ignored class="success" for zebra-striped table rows.

--- parcels/tests.py
+++ parcels/tests.py
@@ -1,23 +1,44 @@
 from django.test import TestCase
 from parcels.utils import scrape_shipment_status
 from parcels.models import Shipment
+from django.contrib.auth.models import User
+
 
 class ScraperTestCase(TestCase):
+
+    def _create_shipment(self, tracking_number):
+        (user, created) = User.objects.get_or_create(
+            username='test', email='test@example.org')
+
+        return Shipment.objects.create(
+            tracking_number=tracking_number, created_user=user)
+
+    def tearDown(self):
+        """
+        Cleans shipments, because of foreign key constraints which
+        will not allow one to add multiple shipments with the same tracking number
+        """
+        Shipment.objects.all().delete()
 
     def test_known_value(self):
         shipment_no = 'RB867569192CN'
         results = scrape_shipment_status(shipment_no)
 
-        self.assertEqual(len(results), 6)
+        self.assertEqual(len(results), 8)
 
     def test_shipments_to_update(self):
-        s = Shipment.objects.create(tracking_number='RB867569192CN')
+        s = self._create_shipment('RB867569192CN')
 
         shipments_to_update = Shipment.objects.shipments_to_update()
         self.assertIn(s, shipments_to_update)
 
     def test_status_entries(self):
-        s = Shipment.objects.create(tracking_number='RB867569192CN')
+        s = self._create_shipment('RB867569192CN')
         s.update()
 
         self.assertGreater(s.statusentry_set.count(), 0)
+
+    def test_shipment_delivered(self):
+        s = self._create_shipment('RW100228572SG')
+        s.update()
+        self.assertTrue(s.is_received)
